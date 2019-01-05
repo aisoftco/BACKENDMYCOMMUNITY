0b8e719918e2cbad26c972dfa1555c7bb24f1e81
wilmerace <wilmerace2@gmail.com>
2019-01-05 15:07:49 -0500

    prueba

---
+++ app/Http/Controllers/VhfController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\vhf;
+use Illuminate\Http\Request;
+
+class VhfController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\vhf  $vhf
+     * @return \Illuminate\Http\Response
+     */
+    public function show(vhf $vhf)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\vhf  $vhf
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(vhf $vhf)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\vhf  $vhf
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, vhf $vhf)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\vhf  $vhf
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(vhf $vhf)
+    {
+        //
+    }
+}
